2d075001318b7e8aab4ac47f32bfa4b2dbc5a3dd
Hasan Yildirim <hadirekt@gmail.com>
2025-03-19 21:24:55 +0100

    log

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  compress: true,
+  images: {
+    domains: ['example.com'], // Add domains for optimized images
+  },
 };
 
 export default nextConfig;
