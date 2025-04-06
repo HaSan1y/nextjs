50317e262e1eaf0cafcf56fe783d7aa2e5c63afd
Hasan Yildirim <hadirekt@gmail.com>
2025-04-06 19:42:34 +0200

    try

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   compress: true,
-  swcMinify: true,
   basePath: '/app',
   images: {
     domains: ['example.com', 'localhost'],
